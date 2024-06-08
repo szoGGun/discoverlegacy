932a48312c5b308ae537fb3bc4bfc9c0d08d73f7
Przemysław Rutkowski <przrut4@gmail.com>
2024-06-08 20:35:38 +0200

    DLEG-2 Initial commit

---
+++ src/main/java/pl/przemyslawrutkowski/discoverlegacy/DiscoverlegacyApplication.java
@@ -0,0 +1,13 @@
+package pl.przemyslawrutkowski.discoverlegacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoverlegacyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoverlegacyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/przemyslawrutkowski/discoverlegacy/DiscoverlegacyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.przemyslawrutkowski.discoverlegacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoverlegacyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
